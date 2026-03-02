9fb0aa319662f5aa8a70b86c4d5763b2f95ce32e
itumeleng-itu <matomempaola@gmail.com>
2026-03-02 11:56:13 +0200

    feat: implement POST endpoint to sort input data

--- server.js
+++ server.js
@@ -3,7 +3,21 @@ import express from "express";
 const app = express();
 
 app.use(express.json());
+const PORT = process.env.PORT || 3000
+
+app.post('/',(req,res)=>{
+    const data= req.body.data
+
+    if(!data) return;
+
+    const sortedArray = data.split("").sort()
+
+    res.status(200).json({
+        original:data,
+        sorted:sortedArray
+    })
+})
 
 app.listen(3000, () => {
-    console.log("Server is running on port 3000");
+    console.log(`Server is running on port http://localhost:${PORT}`);
 });
